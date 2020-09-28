0725ae717afdf3b541b1c6fea3f06cb3f23f75a4
MoscowskyAnton <moscowskyad@gmail.com>
2020-09-29 01:01:52 +0300

    fixed graph

--- src/rerol_vs_add.py
+++ src/rerol_vs_add.py
@@ -8,10 +8,14 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__' :
     
     N = 10000
+    results_base = np.zeros(4)
     results_rr = np.zeros(4)
     results_a = np.zeros(4)
     for i in range(3,7):
         for n in range(N):
+            base = ac_core.d6()
+            if base >= i:
+                results_base[i-3] += 1
             rr = ac_core.d6rr(i-1)
             if rr >= i:
                 results_rr[i-3] += 1            
@@ -19,12 +23,15 @@ if __name__ == '__main__' :
             if a >= i-1:
                 results_a[i-3] += 1            
                 
+    results_base /= N
     results_a /= N
     results_rr /= N
     
+    plt.plot([3,4,5,6], results_base, 'o-', label="base")
     plt.plot([3,4,5,6], results_a, 'o-', label="add_to_hit")
     plt.plot([3,4,5,6], results_rr, 'o-', label="reroll")
     plt.xticks([3,4,5,6],["3+","4+","5+","6+"])
+    plt.yticks(np.arange(0,1.1,0.1))
     plt.legend()
     plt.title("Reroll vs add to hit")
     plt.xlabel("Requred roll")
