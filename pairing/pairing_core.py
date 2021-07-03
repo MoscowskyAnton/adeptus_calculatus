51f65b70e52b307ef2283c06ec90b0ca94c53fbf
anton <moscowsky>
2021-07-03 23:20:27 +0300

    added random moves

--- pairing/pairing_core.py
+++ pairing/pairing_core.py
@@ -73,6 +73,9 @@ class PairingGame(object):
         return score
     
     def print_results(self):
+        sc = self.get_score()
+        print("Total score A {} : {} B".format(sc, self.PT.max_team_score - sc))
+        
         print("DefA ({}) vs AtackB ({}) - {}".format(self.team_A_def, self.team_B_choosed_atacker, self.PT[self.team_A_def, self.team_B_choosed_atacker]))
         
         print("AtackA ({}) vs DefB ({}) - {}".format( self.team_A_choosed_atacker, self.team_B_def, self.PT[ self.team_A_choosed_atacker,self.team_B_def]))
@@ -80,58 +83,122 @@ class PairingGame(object):
         print("RejA ({}) vs RejB ({}) - {}".format(self.team_A_reject_atacker, self.team_B_reject_atacker, self.PT[self.team_A_reject_atacker, self.team_B_reject_atacker]))
         
         print("ChampA ({}) vs ChampB ({}) - {}".format(self.team_A_champion, self.team_B_champion, self.PT[self.team_A_champion, self.team_B_champion]))
+        
+    def set_def(self, team, player_i):
+        if team == 'A':
+            self.team_A_def = player_i
+            self.team_A_state[player_i] = False
+        elif team == 'B':
+            self.team_B_def = player_i
+            self.team_B_state[player_i] = False
+    
+    def release_def(self, team):
+        if team == 'A':
+            defender = self.team_A_def
+            self.team_A_def = None
+            self.team_A_state[defender] = True
+        elif team == 'B':
+            defender = self.team_B_def
+            self.team_B_def = None
+            self.team_B_state[defender] = True
+        return defender
+    
+    def set_atackers(self, team, a1, a2, also_champ = False):
+        if team == 'A':
+            self.team_A_atackers = [a1, a2]
+            self.team_A_state[a1] = False
+            self.team_A_state[a2] = False
+            if also_champ:
+                self.team_A_champion = self.team_A_state.index(True)
+                self.team_A_state[self.team_A_champion] = False
+                
+        elif team == 'B':
+            self.team_B_atackers = [a1, a2]
+            self.team_B_state[a1] = False
+            self.team_B_state[a2] = False
+            if also_champ:
+                self.team_B_champion = self.team_B_state.index(True)
+                self.team_B_state[self.team_B_champion] = False
+            
+    def release_atackers(self, team, also_champ = False):
+        if team == 'A':
+            atackers = self.team_A_atackers
+            self.team_A_state[atackers[0]] = True
+            self.team_A_state[atackers[1]] = True
+            self.team_A_atackers = None
+            if also_champ:
+                self.team_A_state[self.team_A_champion] = True
+                self.team_A_champion = None
+        elif team == 'B':
+            atackers = self.team_B_atackers
+            self.team_B_state[atackers[0]] = True
+            self.team_B_state[atackers[1]] = True
+            self.team_B_atackers = None
+            if also_champ:
+                self.team_B_state[self.team_B_champion] = True
+                self.team_B_champion = None
+        return atackers
+    
+    def choose_atacker(self, team, choosed_player, rejected_player = None):
+        if team == 'B':
+            self.team_B_choosed_atacker = choosed_player
+            if not rejected_player is None:
+                self.team_B_reject_atacker = rejected_player
+            else:
+                self.team_B_reject_atacker = [x for x in self.team_B_atackers if x != self.team_B_choosed_atacker][0]
+                
+            # state should be alredy false
+        elif team == 'A':
+            self.team_A_choosed_atacker = choosed_player
+            if not rejected_player is None:
+                self.team_A_reject_atacker = rejected_player
+            else:
+                self.team_A_reject_atacker = [x for x in self.team_A_atackers if x != self.team_A_choosed_atacker][0]
+    
+    def unchoose_atacker(self, team):
+        if team == 'A':
+            self.team_A_choosed_atacker = None
+            self.team_A_reject_atacker = None
+        elif team == 'B':
+            self.team_B_choosed_atacker = None
+            self.team_B_reject_atacker = None
+        
               
     # team A turn
     def max(self, state):
         total_score = self.PT.min_team_score
         player_selected = None
         
-        #if self.game_seq[self.game_state] == 'def':
         if state == 'def':
             for player_i, player_free in enumerate(self.team_A_state):
                 if player_free:
-                    self.team_A_state[player_i] = False
-                    self.team_A_def = player_i
+                    self.set_def('A', player_i)
                     sc, pl = self.min(state)
-                    
                     if sc >= total_score:
                         total_score = sc
                         player_selected = player_i
+                    self.release_def('A')
                     
-                    self.team_A_state[player_i] = True
-                    self.team_A_def = None
-                    
-        #elif self.game_seq[self.game_state] == 'atacks':
         elif state == 'atacks':
             for player_ii, player_i_free in enumerate(self.team_A_state):
                 for player_jj, player_j_free in enumerate(self.team_A_state):
                     if player_ii != player_jj and player_i_free and player_j_free:
                         
-                        self.team_A_atackers = [player_ii, player_jj]
-                        self.team_A_state[player_ii] = False
-                        self.team_A_state[player_jj] = False
-                        self.team_A_champion = self.team_A_state.index(True)
-                        self.team_A_state[self.team_A_champion] = False
+                        self.set_atackers('A', player_ii, player_jj, True)
                         
                         sc, pl = self.min(state)
                         if sc >= total_score:
                             total_score = sc
                             player_selected = [player_ii, player_jj]
                             
-                        self.team_A_atackers = None
-                        self.team_A_state[player_ii] = True
-                        self.team_A_state[player_jj] = True
-                        self.team_A_state[self.team_A_champion] = True
-                        self.team_A_champion = None
+                        self.release_atackers('A', True)
+                        
             
-        #elif self.game_seq[self.game_state] == 'chooseAtack':
         elif state == 'chooseAtack':
             atackers = [(self.team_B_atackers[0], self.team_B_atackers[1]),
                         (self.team_B_atackers[1], self.team_B_atackers[0])]
             for i, j in atackers:
-                #print(i,j, atackers)
-                self.team_B_choosed_atacker = i
-                self.team_B_reject_atacker = j
+                self.choose_atacker('B', i, j)# yes B
                 
                 sc, pl = self.min(state)
                 
@@ -139,8 +206,7 @@ class PairingGame(object):
                     total_score = sc
                     player_selected = i
                 
-                self.team_B_choosed_atacker = None
-                self.team_B_reject_atacker = None
+                self.unchoose_atacker('B')
         
         return total_score, player_selected
      
@@ -149,111 +215,138 @@ class PairingGame(object):
         total_score = self.PT.max_team_score
         player_selected = None
         
-        #if self.game_seq[self.game_state] == 'def':
         if state == 'def':
-            #self.game_state += 1
             for player_i, player_free in enumerate(self.team_B_state):
                 if player_free:
-                    self.team_B_state[player_i] = False
-                    self.team_B_def = player_i
-                    
+                    self.set_def('B', player_i)
                     sc, pl = self.max('atacks')
                     
                     if sc <= total_score:
                         player_selected = player_i
                         total_score = sc
                     
-                    self.team_B_state[player_i] = True
-                    self.team_B_def = None
+                    self.release_def('B')
                     
-        #elif self.game_seq[self.game_state] == 'atacks':
         elif state == 'atacks':
-            #self.game_state += 1
             for player_ii, player_i_free in enumerate(self.team_B_state):
                 for player_jj, player_j_free in enumerate(self.team_B_state):
                     if player_ii != player_jj and player_i_free and player_j_free:
                         
-                        self.team_B_atackers = [player_ii, player_jj]
-                        self.team_B_state[player_ii] = False
-                        self.team_B_state[player_jj] = False
-                        self.team_B_champion = self.team_B_state.index(True)
-                        self.team_B_state[self.team_B_champion] = False
-                        #print('tba',self.team_B_atackers)
+                        self.set_atackers('B', player_ii, player_jj, True)
+                        
                         sc, pl = self.max('chooseAtack')
                         if sc <= total_score:
                             total_score = sc
                             player_selected = [player_ii, player_jj]
                             
-                        self.team_B_atackers = None
-                        self.team_B_state[player_ii] = True
-                        self.team_B_state[player_jj] = True
-                        self.team_B_state[self.team_B_champion] = True
-                        self.team_B_champion = None
-                    
-        #elif self.game_seq[self.game_state] == 'chooseAtack':
+                        self.release_atackers('B', True)
+                        
         elif state == 'chooseAtack':
-            #self.game_state += 1
             atackers = [(self.team_A_atackers[0], self.team_A_atackers[1]),
                         (self.team_A_atackers[1], self.team_A_atackers[0])]
             for i, j in atackers:
-                #print(i,j)
-                self.team_A_choosed_atacker = i
-                self.team_A_reject_atacker = j
+                self.choose_atacker('A', i, j) # yes, A
                 
                 sc = self.get_score()
-                #print(sc, total_score)
                 if sc <= total_score:
                     player_selected = i
                     total_score = sc
-                
-                self.team_A_choosed_atacker = None
-                self.team_A_reject_atacker = None
+                    
+                self.unchoose_atacker('A')
+        else:
+            print('ERROR! unknown phase {}'.format(phase))        
         
         return total_score, player_selected
     
+    def make_optimal_move(self, team, phase):
+        if phase == 'def':
+            if team == 'A':
+                score, defender = self.max('def')
+            elif team == 'B':
+                score, defender = self.min('def')
+            self.set_def(team, defender)
+            print('TEAM {}: recomend to set defender {} for max score {}'.format(team, defender, score))
+        elif phase == 'atacks':
+            if team == 'A':
+                score, atackers = self.max('atacks')
+            elif team == 'B':
+                score, atackers = self.min('atacks')
+            self.set_atackers(team, atackers[0], atackers[1], True)
+            print('TEAM {}: recomend to set atackers {} for max score {}'.format(team, atackers, score))
+        elif phase == 'chooseAtack':
+            if team == 'A':
+                score, choosed = self.max('chooseAtack')
+                self.choose_atacker('B', choosed, None)
+            elif team == 'B':
+                score, choosed = self.min('chooseAtack')
+                self.choose_atacker('A', choosed, None)
+            print('TEAM {}: recomend choose player {} from oppsite atackers, for score {}'.format(team, choosed, score))
+        else:
+            print('ERROR! unknown phase {}'.format(phase))
+        return score
+    
+    def random_choose_free_player(self, team):
+        if team == 'A':
+            while(True):
+                player = np.random.randint(0,4)
+                if self.team_A_state[player]:
+                    return player
+        elif team == 'B':
+            while(True):
+                player = np.random.randint(0,4)
+                if self.team_B_state[player]:
+                    return player
+    
+    def make_random_move(self, team, phase):
+        if phase == 'def':
+            defender = self.random_choose_free_player(team)
+            self.set_def(team, defender)
+            print("TEAM {} randomly set player {} as defender".format(team, defender))
+        elif phase == 'atacks':
+            atack1 = self.random_choose_free_player(team)
+            while 1:
+                atack2 = self.random_choose_free_player(team)
+                if atack1 != atack2:
+                    break
+            self.set_atackers(team, atack1, atack2, True)
+            print('TEAM {} randomly set players {} as atackers'.format(team, (atack1, atack2)))
+            
+        elif phase == 'chooseAtack':
+            choose = np.random.randint(0,2)
+            if team == 'A':
+                pl = self.team_B_atackers[choose]
+                self.choose_atacker('B', pl)
+            elif team == 'B':
+                pl = self.team_A_atackers[choose]
+                self.choose_atacker('A', pl)
+            print('TEAM {} randomly choosed player {} from opposite atackers'.format(team, pl))
+    
     def play_optimal_way(self):
-        print(self.PT)
-        print('TEAM A mean score = {}'.format(self.PT.mean()))
         
-        score, defender = self.max('def')
-        print('TEAM A: defend with player A{} and team A score will be {}'.format(defender, score))
-        self.team_A_def = defender
-        self.team_A_state[defender] = False
-        
-        score, defender = self.min('def')
-        print('TEAM B: defend with player B{} and team A score will be {}'.format(defender, score))
-        self.team_B_def = defender
-        self.team_B_state[defender] = False
-        
-        score, atackers = self.max('atacks')
-        print('TEAM A: atack with players A{} and team A score will be {}'.format(atackers, score))
-        self.team_A_atackers = atackers#list(atackers)
-        self.team_A_state[atackers[0]] = False
-        self.team_A_state[atackers[1]] = False
-        self.team_A_champion = self.team_A_state.index(True)
-        self.team_A_state[self.team_A_champion] = False
-        
-        score, atackers = self.min('atacks')
-        print('TEAM B: atack with players B{} and team A score will be {}'.format(atackers, score))
-        self.team_B_atackers = atackers
-        self.team_B_state[atackers[0]] = False
-        self.team_B_state[atackers[1]] = False
-        self.team_B_champion = self.team_B_state.index(True)
-        self.team_B_state[self.team_B_champion] = False
-        
-        score, choose = self.max('chooseAtack')
-        print('TEAM A: chooose player B{} and team A score will be {}'.format(choose, score))
-        self.team_B_choosed_atacker = choose
-        self.team_B_reject_atacker = [x for x in self.team_B_atackers if x != self.team_B_choosed_atacker][0]
-        
-        score, choose = self.min('chooseAtack')
-        print('TEAM B: chooose player A{} and team A score will be {}'.format(choose, score))
-        self.team_A_choosed_atacker = choose
-        self.team_A_reject_atacker = [x for x in self.team_A_atackers if x != self.team_A_choosed_atacker][0]
-        
-        self.print_results()
+        self.make_optimal_move('A', 'def')
+        self.make_optimal_move('B', 'def')
+        self.make_optimal_move('A', 'atacks')
+        self.make_optimal_move('B', 'atacks')
+        self.make_optimal_move('A', 'chooseAtack')
+        score = self.make_optimal_move('B', 'chooseAtack')
         
         return score
+    
+    def play_random(self):
+        self.make_random_move('A', 'def')
+        self.make_random_move('B', 'def')
+        self.make_random_move('A', 'atacks')
+        self.make_random_move('B', 'atacks')
+        self.make_random_move('A', 'chooseAtack')
+        self.make_random_move('B', 'chooseAtack')
+        
+    def play_optimal_vs_random(self):
+        self.make_optimal_move('A', 'def')
+        self.make_random_move('B', 'def')
+        self.make_optimal_move('A', 'atacks')
+        self.make_random_move('B', 'atacks')
+        self.make_optimal_move('A', 'chooseAtack')
+        self.make_random_move('B', 'chooseAtack')
         
 if __name__ == '__main__' :
     
@@ -264,6 +357,22 @@ if __name__ == '__main__' :
                                 [20,20,0,0]]))
     '''
     pt = PairingTable(np.random.randint(0,21,(4,4)))
-    
     pg = PairingGame(pt)
+    
+    print(pg.PT)
+    print('TEAM A mean score = {}'.format(pg.PT.mean()))
+    
     pg.play_optimal_way()
+    score_optimal = pg.get_score()
+    pg.print_results()
+    
+    scores_optimal_vs_random = []
+    N = 10
+    for n in range(N):
+        pg.reset()
+        pg.play_optimal_vs_random()
+        scores_optimal_vs_random.append(pg.get_score())
+        pg.print_results()
+        
+    print(score_optimal, scores_optimal_vs_random)
+        
