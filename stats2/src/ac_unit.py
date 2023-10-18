d06fd9e94c9de3d6b2dc54c0f8e661036e2b7596
moscowsky <moscowskyad@gmail.com>
2023-10-18 19:39:36 +0300

    many was changed

--- stats2/src/ac_unit.py
+++ stats2/src/ac_unit.py
@@ -2,38 +2,83 @@
 # coding: utf-8
 import ac_weapon
 from enum import Enum, auto
+import copy
+import matplotlib.pyplot as plt
 
-class AC_UNIT_TYPE(Enum):
-    INFATRY = auto()
-    VEHICLE = auto()
-    CAVALERY = auto()
-    OTHER = auto()
-
-class AC_SHOOT_ATACKER(object):
-
-    def __init__(self, bs, **abilities):
-        self.bs = bs
-        
-        self.abilities = abilities
+class AC_UNIT(object):
+    
+    IN_COVER = 'IN_COVER'
+    
+    def __init__(self, movement, toughness, save, invul, wounds, leadership, oc, weapons = [], *args, **kwargs):
+            
+        self.movement = movement
+        self.toughness = toughness
+        self.save = save
+        self.invul = invul
+        self.wounds = wounds
+        self.leadership = leadership
+        self.oc = oc
         
         self.weapons = []
-
-    '''
-    Registes a weapon for model
-    '''
-    def give_weapon(self, weapon):
-        if not isinstance(weapon, ac_weapon.AC_WEAPON):
-            raise ValueError("Weapon must be AC_WEAPON")
+        for wep in weapons:
+            self.weapons.append(copy.copy(wep))
         
-        self.weapons.append(weapon)
-        
-
-class AC_DEFENDER(object):
+        self.args_abilities = args
+        self.kwargs_abilities = kwargs
     
-    def __init__(self, toughness, armour, invul = None, **abilities):        
-        self.toughness = toughness
-        self.armour = armour
-        self.invul = invul
+    
+    def attack_target(self, target, range_):
         
-        self.abilities = abilities
+        total_damage = 0
+        for weapon in self.weapons:
+            total_damage += weapon.get_damage(target, range_)
+            
+        return total_damage
+            
+    
+    def mean_damage_target(self, target, range_, N):
+        td = 0
+        for i in range(N):
+            td += self.attack_target(target, range_)
+        return td / N
+    
+    def set_data_damage_target(self, target, range_, N):
+        data = []
+        for i in range(N):
+            data.append(self.attack_target(target, range_))
+        return data
+        
+    
+if __name__ == '__main__':
+    
+    # weapons
+    LasCannon = ac_weapon.AC_WEAPON(48, 1, 4, 12, 3, 'd6+1')
+    MultiMelta = ac_weapon.AC_WEAPON(18, 2, 4, 9, 4, 'd6', MELTA = 2)
+    HeavyStubber = ac_weapon.AC_WEAPON(36, 3, 4, 4, 0, 1, RAPID_FIRE = 3)
+    DemolisherCannon = ac_weapon.AC_WEAPON(24, 'd6+3', 4, 14, 3, 'd6')
+    HunterKillerMissile = ac_weapon.AC_WEAPON(48, 1, 4, 14, 3, 'd6')
+    
+    OpressorCannon = ac_weapon.AC_WEAPON(60, 'd6+3', 4, 12, 2, 3)
+    CoaxialAutoCannon = ac_weapon.AC_WEAPON(48, 2, 3, 9, 1, 3)
+    PulziverCannon = ac_weapon.AC_WEAPON(24, 'd6', 4, 9, 3, 3)
+    
+    # units
+    LemanRussDemolisher = AC_UNIT(10, 11, 2, 0, 13, 7, 3, [DemolisherCannon, LasCannon, MultiMelta, MultiMelta, HeavyStubber, HunterKillerMissile])
+    
+    RogalDornBattleTank = AC_UNIT(10, 12, 2, 0, 18, 7, 5, [OpressorCannon, CoaxialAutoCannon, MultiMelta, MultiMelta, HeavyStubber, HeavyStubber, HeavyStubber, PulziverCannon])
+    
+    range_ = 18
+    N = 1000
+    
+    #print(f'Damage of RogalDorn on LemanRussDemolisher at {range_}\" is {RogalDornBattleTank.mean_damage_target(LemanRussDemolisher, range_, N)}')
+    
+    #print(f'Damage of LemanRussDemolisher on RogalDorn at {range_}\" is {LemanRussDemolisher.mean_damage_target(RogalDornBattleTank, range_, N)}')
+
+
+    
+    d1 = RogalDornBattleTank.set_data_damage_target(LemanRussDemolisher, range_, N)
+    d2 = LemanRussDemolisher.set_data_damage_target(RogalDornBattleTank, range_, N)
+    
+    plt.violinplot([d1, d2], showmeans = True, showextrema = False)
+    plt.show()
         
--- stats2/src/ac_weapon.py
+++ stats2/src/ac_weapon.py
@@ -3,32 +3,42 @@
 
 import numpy as np
 from enum import Enum, auto
-from ac_regular import AC_REGULAR
+import ac_regular
         
-
-class AC_WEAPON_TYPE(Enum):
-    HEAVY = auto()
-    ASSAULT = auto()
-    RAPID_FIRE = auto()
-    PISTOL = auto()
-    MELEE = auto()
-    OTHER = auto()
-    
-    def __str__(self):
-        return self.name
+AC_MOVE = Enum('MOVE', ['NORMAL', 'REMAIN_STATIONARY', 'FOLL_BACK', 'ADVANCE'])
 
 class AC_WEAPON(object):        
     
-    def __init__(self, type_, shots, range_, strength, ap, **abilities):
-        if not isinstance(type_, AC_WEAPON_TYPE):
-            raise TypeError('AC_WEAPON.__init__: type_ must be an AC_WEAPON_TYPE')
-        self.type = type_
-        if isinstance(shots, int):
-            self.shots = shots
-        elif isinstance(shots, str):
-            self.shots = AC_REGULAR(shots)
+    RAPID_FIRE = 'RAPID_FIRE'
+    TORRENT = 'TORRENT'
+    MELTA = 'MELTA'
+    LETHAL_HITS = 'LETHAL_HITS'
+    REROLL_TO_HIT = 'REROLL_TO_HIT'
+    DEVASTATING_WOUNDS = 'DEVASTATING_WOUNDS'
+    MOVEMENT_TYPE = 'MOVEMENT_TYPE'
+    SUSTANED_HITS = 'SUSTANED_HITS'
+    IGNORES_COVER = 'IGNORES_COVER'
+    
+    '''
+    Constructs weapon class
+    args:
+        range_ - max range of weapon 
+        attacks - number of attacks, can be like 3d3+2
+        skill - ballistic or weapon skill, int
+        strength - 
+        ap - 
+        damage - damage, can be like 2d6
+        *args - abilities without parameters, like TORRENT, ASSAULT, HEAVY etc.
+        **kwargs - abilities with paameters, like RAPID_FIRE 1, SUSTANED_HITS 2 etc.
+    '''
+    def __init__(self, range_, attacks, skill, strength, ap, damage, *args, **kwargs):
+        
+        if isinstance(attacks, int):
+            self.attacks = attacks
+        elif isinstance(attacks, str):
+            self.attacks = ac_regular.AC_REGULAR(attacks)
         else:
-            raise TypeError('AC_WEAPON.__init__: shots must be an integer or tring')                
+            raise TypeError('AC_WEAPON.__init__: attacks must be an integer or string')                
         if not isinstance(range_, int):
             raise TypeError('AC_WEAPON.__init__: range_ must be an integer')
         self.range = range_
@@ -37,35 +47,161 @@ class AC_WEAPON(object):
         self.strength = strength
         if not isinstance(ap, int):
             raise TypeError('AC_WEAPON.__init__: ap must be an integer')
-        self.ap = np.abs(ap)
+        self.ap = np.abs(ap) # if some one put -2
         
-        self.abilities = abilities
+        if isinstance(damage, int):
+            self.damage = damage
+        elif isinstance(damage, str):
+            self.damage = ac_regular.AC_REGULAR(damage)
+        else:
+            raise TypeError('AC_WEAPON.__init__: damage must be an integer or string')
+        
+        if isinstance(skill, int):
+            self.skill = skill
+        else:
+            raise TypeError('AC_WEAPON.__init__: skill must be integer')
+        
+        self.critical_hit = 6
+        self.critical_wound = 6
+        
+        self.args_abilities = args
+        self.kwargs_abilities = kwargs
+        
             
+    def _attacks(self):
+        if isinstance(self.attacks, int):
+            return self.attacks
+        else:
+            return self.attacks.roll()
+    
+    
+    def _damage(self):
+        if isinstance(self.damage, int):
+            return self.damage
+        else:
+            return self.damage.roll()
+    
     '''
-    Gives number of shots from weapon
+    Gives number of attacks from weapon
     args:
-        range_ 
+        range_ - distance to target
     '''
-    def get_shots(self, range_ = 0):
+    def get_attacks(self, range_ = 0):
         if range_ > self.range:
             return 0
-        if self.type != AC_WEAPON_TYPE.RAPID_FIRE or range_ > self.range /2 :            
-            if isinstance(self.shots, int):
-                return self.shots
-            else:
-                return self.shots.roll()
+        if not AC_WEAPON.RAPID_FIRE in self.kwargs_abilities or range_ > self.range /2 :            
+            return self._attacks()
         else:
-            if isinstance(self.shots, int):
-                return self.shots*2
-            else:
-                return self.shots.roll()*2 # ???
+            return self._attacks()+self.kwargs_abilities[AC_WEAPON.RAPID_FIRE]
         
+    '''
+    Gives number of hits
+    args:
+        range_ - distance to target
+    returns:
+        hits - 
+        wounds - if LETHAL_HITS are here
+    '''
+    def get_hits(self, range_ = 0):
+        attacks = self.get_attacks(range_)
+        
+        hits = 0
+        wounds = 0
+        value_to_hit = self.skill
+        # TODO modifiers
+        
+        if AC_WEAPON.TORRENT in self.args_abilities:
+            return attacks, 0
+        
+        for i in range(attacks):
+            die = ac_regular.roll_d6()
+            # TODO rr here
+            
+            if( die != 1):
+                
+                if die >= self.critical_hit:
+                    if AC_WEAPON.LETHAL_HITS in self.args_abilities:
+                        wounds += 1
+                    else:
+                        hits += 1
+                    if AC_WEAPON.SUSTANED_HITS in self.kwargs_abilities:
+                        hits += self.kwargs_abilities[AC_WEAPON.SUSTANED_HITS]
+                elif die >= value_to_hit:
+                    hits += 1
+        
+        #print(hits, wounds)
+        return hits, wounds
 
-
+    '''
+    get wounds
+    '''
+    def get_wounds(self, target, range_ = 0):
+        hits, wounds = self.get_hits(range_)
+        
+        saves, no_saves = 0, 0
+        
+        if self.strength *2 <= target.toughness:
+            to_wound_value = 6
+        elif self.strength < target.toughness:
+            to_wound_value = 5
+        elif self.strength == target.toughness:
+            to_wound_value = 4
+        elif self.strength >= target.toughness * 2:
+            to_wound_value = 2
+        else:
+            to_wound_value = 3
+        
+        # TODO modifiers
+        
+        for i in range(hits):
+            
+            die = ac_regular.roll_d6()
+            # TODO RR
+            if die != 1:
+                if die >= self.critical_wound:
+                    if AC_WEAPON.DEVASTATING_WOUNDS in self.args_abilities:
+                        no_saves += 1
+                    else:
+                        saves += 1
+                elif die >= to_wound_value:
+                    saves += 1
+        
+        #print(saves)
+        return saves, no_saves
+    
+    def get_damage(self, target, range_ = 0):
+        
+        damage = 0
+        saves, no_saves = self.get_wounds(target, range_)
+        damages = no_saves
+        save = target.save + self.ap # TODO cover
+        if target.invul != 0:
+            save = min(save, target.invul)
+        
+        for i in range(saves):
+            die = ac_regular.roll_d6()
+            
+            if die == 1 or die < save:
+                damages += 1
+                
+                
+        for i in range(damages):   
+            damage += self._damage()
+            if AC_WEAPON.MELTA in self.kwargs_abilities and range_ <= self.range/2:
+                damage += self.kwargs_abilities[AC_WEAPON.MELTA]
+        
+        #TODO FNP
+        
+        return damage
+        
+            
 if __name__ == '__main__' :
     
-    hot_shot_lasgun = AC_WEAPON(AC_WEAPON_TYPE.RAPID_FIRE, 1, 24, 3, 2)
-    print(hot_shot_lasgun.get_shots(11))
+    lasgun = AC_WEAPON(24, 1, 4, 3, 0, 1, RAPID_FIRE = 1)
+    #print(lasgun.get_attacks(6))
+    print(lasgun.get_hits())
     
-    flamer = AC_WEAPON(AC_WEAPON_TYPE.ASSAULT, 'd6', 12, 3, 0)
-    print(flamer.get_shots(13))
+    
+    flamer = AC_WEAPON(12, 'd6', 0, 4, 0, 1, AC_WEAPON.TORRENT)
+    #print(flamer.get_attacks(1))
+    print(flamer.get_hits())
