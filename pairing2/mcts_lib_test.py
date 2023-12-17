be12c2a229884e6819c5f9bdd4238f87be15b7c0
moscowsky <moscowskyad@gmail.com>
2023-12-17 21:42:56 +0300

    always 0

--- pairing2/mcts_lib_test.py
+++ pairing2/mcts_lib_test.py
@@ -6,10 +6,10 @@ from copy import deepcopy
 GameStage = Enum('GameStage', ['DEF', 'ATTCK', 'CHOOSE', 'TABLE', 'FIN'])
 
 
-SCORES = np.random.randint(-2, 3, size= (4, 4, 3))
+SCORES = np.random.randint(-2, 3, size= (4, 4, 4))
 
 # team stage_type pairing no 
-SEQUENCE = [(0, GameStage.DEF, 0),
+SEQUENCE = np.array([(0, GameStage.DEF, 0),
             (1, GameStage.DEF, 1),
             (0, GameStage.ATTCK, 0),
             (1, GameStage.ATTCK, 1),
@@ -17,7 +17,7 @@ SEQUENCE = [(0, GameStage.DEF, 0),
             (1, GameStage.CHOOSE, 1),
             (0, GameStage.TABLE, 0),
             (1, GameStage.TABLE, 1),
-            (1, GameStage.FIN, 2)]
+            (1, GameStage.FIN, 2)])
 
 class Team(object):
     
@@ -49,7 +49,7 @@ class GameState():
         self.free_tables = [0, 1, 2, 3]#, 4, 5, 6, 7]
         self.table_types = [0, 1, 1, 2]#, 3, 3, 3, 3]
         
-        self.pairings = np.ones((4, 3)) * -1
+        self.pairings = np.ones((4, 3), dtype = int) * -1
         '''
         def1 atack2 t
         def2 atack1 t
@@ -102,29 +102,34 @@ class GameState():
             new_state.teams[team].free_players.remove(action.choise1)
             
         elif stage_type == GameStage.ATTCK:
-            new_state.teams[team].atacks = (action.choise1, action.choise2)
+            new_state.teams[team].atacks = [action.choise1, action.choise2]
             new_state.teams[team].free_players.remove(action.choise1)
             new_state.teams[team].free_players.remove(action.choise2)
             
         elif stage_type == GameStage.CHOOSE:            
+            #print(action.choise1, new_state.teams[swap_teams(team)].atacks)
             new_state.pairings[pairing_no][swap_teams(team)] = action.choise1
-            new_state.teams[swap_teams(team)].free_players.remove(action.choise1)
-            new_state.teams[swap_teams(team)].rej = teams[swap_teams(team)].atacks.remove(action.choise1)[0]
+            #new_state.teams[swap_teams(team)].free_players.remove(action.choise1)
+            new_state.teams[swap_teams(team)].atacks.remove(action.choise1)
+            #print(new_state.teams[swap_teams(team)].atacks)
+            new_state.teams[swap_teams(team)].rej = new_state.teams[swap_teams(team)].atacks[0]
             
         elif stage_type == GameStage.TABLE:
             new_state.pairings[pairing_no][2] = action.choise1
             new_state.free_tables.remove(action.choise1)
             
-        elif stage_type == GameStage.FIN:
+        elif stage_type == GameStage.FIN:            
             
-            
-            new_state.pairings[2] = np.array(teams[0].rej, teams[1].rej, action.choise1)
+            new_state.pairings[2] = np.array([new_state.teams[0].rej, new_state.teams[1].rej, action.choise1])
             
             ch1 = new_state.teams[0].free_players[0]
             ch2 = new_state.teams[1].free_players[0]
             new_state.free_tables.remove(action.choise1)
-            new_state.pairings[3] = np.array(ch1, ch2, new_state.free_tables[0])
-            
+            new_state.pairings[3] = np.array([ch1, ch2, new_state.free_tables[0]])
+        
+        #print(SEQUENCE[self.stage_num])
+        #print(new_state.pairings)
+        new_state.stage_num+=1
         
         return new_state
             
@@ -135,20 +140,40 @@ class GameState():
     def getReward(self):
         reward = 0
         for i in [0, 2, 3]:
-            reward += SCORES[self.pairing[i]]
-        reward += SCORES[self.pairing[1][1], self.pairing[1][0], self.pairing[1][2]]
+            #print(self.pairings[i])
+            #print(self.pairings[i], SCORES[self.pairings[i]])
+            #exit()
+            #reward += SCORES[self.pairings[i]]
+            reward += SCORES[self.pairings[i][0], 
+                             self.pairings[i][1], 
+                             self.pairings[i][2]]
+        #print(self.pairings, SCORES.shape)
+        reward += SCORES[self.pairings[1][1], 
+                         self.pairings[1][0], 
+                         self.pairings[1][2]]
+        #print(reward)
         return reward
+    
+    def __eq__(self, other):
+        raise NotImplementedError()
     
 class Action():
     
-    def __init__(self, sequnce, choise1, choise2 = -1):
-        
+    def __init__(self, sequnce, choise1, choise2 = -1):        
         self.sequnce = sequnce
         self.choise1 = choise1
         self.choise2  = choise2
         
+    def __str__(self):
+        return f'{self.sequnce}: {self.choise1} {self.choise2}'
+        
     def __hash__(self):
-        return hash((team, game_stage, choise1, choise2))
+        #return hash((team, game_stage, choise1, choise2))
+        return hash((self.sequnce[0], self.sequnce[1], self.choise1, self.choise2))
+    
+    def __eq__(self, other):
+        #raise NotImplementedError()
+        return False
     
     
 if __name__ == '__main__':    
