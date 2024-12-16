1b17e491acaaf324bf3036a711661073a2397bff
moscowsky <moscowskyad@gmail.com>
2024-12-16 13:39:29 +0300

    update letahls vs +wound

--- stats/src/lethal_vs_plus2wound.py
+++ stats/src/lethal_vs_plus2wound.py
@@ -19,11 +19,13 @@ if __name__ == '__main__' :
             
             all_lethal_cnt = []
             all_plus_wound_cnt = []
+            all_nothing_cnt = []
             
             for to_wound in to_wounds:
                 
                 lethal_cnt = []
                 plus_wound_cnt = []
+                nothing_cnt = []
                 
                 for n in range(N):
                     if full_reroll:
@@ -50,37 +52,59 @@ if __name__ == '__main__' :
                             plus_wound_cnt.append(1)
                         else:
                             plus_wound_cnt.append(0)
+                            
+                        if wound_roll >= to_wound:
+                            nothing_cnt.append(1)
+                        else:
+                            nothing_cnt.append(0)
                     else:
-                        plus_wound_cnt.append(0)
-                        #if hit_roll != 6:                            
+                        plus_wound_cnt.append(0)                                                 
                         lethal_cnt.append(0)
+                        nothing_cnt.append(0)
                                                                         
                             
                 all_lethal_cnt.append(lethal_cnt)
                 all_plus_wound_cnt.append(plus_wound_cnt)
-            
-            #print([len(a) for a in all_lethal_cnt])
-            #print([len(a) for a in all_plus_wound_cnt])
+                all_nothing_cnt.append(nothing_cnt)
+                        
             
             name = f"lethal_vs_plus_wound_{to_hit}_{full_reroll}"
             plt.figure(name)
-            plt.title(f"Scions Lethals vs Plus Wound\n (to hit={to_hit}), full reroll={full_reroll}")
+            plt.title(f"Scions Lethals vs Plus Wound\nto hit={to_hit}, full reroll={full_reroll}")
             
             all_lethal_cnt = np.array(all_lethal_cnt)
             all_plus_wound_cnt = np.array(all_plus_wound_cnt)
+            all_nothing_cnt = np.array(all_nothing_cnt)
             
             mean_lethals = np.mean(all_lethal_cnt, axis = 1)
-            mean_plus_wound = np.mean(all_plus_wound_cnt, axis = 1) 
+            mean_plus_wound = np.mean(all_plus_wound_cnt, axis = 1)
+            mean_nothing = np.mean(all_nothing_cnt, axis = 1)
             
             std_lethals = np.std(all_lethal_cnt, axis = 1)
             std_plus_wound = np.std(all_plus_wound_cnt, axis = 1)             
             
-            plt.plot(mean_lethals*100, label = "Lethals")
-            plt.plot(mean_plus_wound*100, label = "Plus wound")
+            r = range(0, len(to_wounds))            
+            
+            plt.plot(mean_lethals*100, '.', label = "Lethals", color = plt.get_cmap('tab10', 10)(0))
+            plt.plot(r, mean_lethals*100, linestyle = ':', color = plt.get_cmap('tab10', 10)(0))
+            
+            plt.plot(mean_plus_wound*100, '.', label = "Plus wound", color = plt.get_cmap('tab10', 10)(1))
+            plt.plot(r, mean_plus_wound*100, linestyle = ':', color = plt.get_cmap('tab10', 10)(1))
+            
+            plt.plot(mean_nothing*100, '.', label = "Nothing", color = plt.get_cmap('tab10', 10)(2))
+            plt.plot(r, mean_nothing*100, linestyle = ':', color = plt.get_cmap('tab10', 10)(2))
+            
+            for i, (lethal, plus_wound) in enumerate(zip(mean_lethals.tolist(), mean_plus_wound.tolist())):
+                if lethal > plus_wound:
+                    diff = (lethal-plus_wound) / plus_wound
+                    plt.text(i, lethal*105, f"+{round(diff*100,2)}%", color = plt.get_cmap('tab10', 10)(0), ha="center")
+                else:
+                    diff = (plus_wound-lethal) / lethal
+                    plt.text(i, plus_wound*105, f"+{round(diff*100,2)}%", color = plt.get_cmap('tab10', 10)(1), ha="center")    
             
             alpha = 0.1
-            plt.fill_between(range(0, len(to_wounds)), 100*(mean_lethals - std_lethals), 100*(mean_lethals + std_lethals), alpha = alpha)
-            plt.fill_between(range(0, len(to_wounds)), 100*(mean_plus_wound - std_plus_wound), 100*(mean_plus_wound + std_plus_wound), alpha = alpha)
+            #plt.fill_between(range(0, len(to_wounds)), 100*(mean_lethals - std_lethals), 100*(mean_lethals + std_lethals), alpha = alpha)
+            #plt.fill_between(range(0, len(to_wounds)), 100*(mean_plus_wound - std_plus_wound), 100*(mean_plus_wound + std_plus_wound), alpha = alpha)
             
             
             plt.grid()
