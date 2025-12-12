a33b1570ef5e833e9fac758308514c49a62eeff6
moscowsky <moscowskyad@andex.ru>
2025-12-12 18:27:06 +0300

    something on scions

--- stats2/src/grizzle_scion_power.py
+++ stats2/src/grizzle_scion_power.py
@@ -13,6 +13,10 @@ if __name__ == '__main__':
     
     
     test_scenarious = {}
+    
+    test_scenarious['BASE'] = ([], 
+                                 {'SUSTANED_HITS': 1, 'REROLL_TO_WOUND': 1})
+    
     test_scenarious['FRFSRF'] = ([ac_weapon.AC_WEAPON.FRFSRF], 
                                  {'SUSTANED_HITS': 1, 'REROLL_TO_WOUND': 1})
     
@@ -34,13 +38,7 @@ if __name__ == '__main__':
     
     LABELS = []
     DATA = []
-    
-    HotShotLasgun = ac_weapon.AC_WEAPON(24, 1, 3, 3, 1, 1, name = "HotShotLasgun", RAPID_FIRE = 1)
-    HotShotVolleyGun = ac_weapon.AC_WEAPON(30, 2, 3, 4, 1, 1, name = "HotShotVolleyGun", RAPID_FIRE = 2)
-    HotShotLaspistol = ac_weapon.AC_WEAPON(12, 1, 3, 3, 1, 1, name = "HotShotLaspistol")
-    PlasmaGun = ac_weapon.AC_WEAPON(24, 1, 3, 8, 3, 2, name = "PlasmaGun", RAPID_FIRE = 1)
-    PlasmaPistol = ac_weapon.AC_WEAPON(12, 1, 3, 8, 3, 2, name = "PlasmaPistol")
-    Meltagun = ac_weapon.AC_WEAPON(12, 1, 3, 9, 4, 'd6', name = "Meltagun", MELTA = 2)
+        
     #print(Meltagun.name)    
     base_stats = [6, 3, 4, 0, 1, 7, 1]
     
@@ -48,7 +46,15 @@ if __name__ == '__main__':
     for name, (abilities, kwabilities) in test_scenarious.items():
         LABELS.append(name)
         
-        if name == 'FRFSRF':
+        HotShotLasgun = ac_weapon.AC_WEAPON(24, 1, 3, 3, 1, 1, name = "HotShotLasgun", RAPID_FIRE = 1)
+        HotShotVolleyGun = ac_weapon.AC_WEAPON(30, 2, 3, 4, 1, 1, name = "HotShotVolleyGun", RAPID_FIRE = 2)
+        HotShotLaspistol = ac_weapon.AC_WEAPON(12, 1, 3, 3, 1, 1, name = "HotShotLaspistol")
+        PlasmaGun = ac_weapon.AC_WEAPON(24, 1, 3, 8, 3, 2, name = "PlasmaGun", RAPID_FIRE = 1)
+        PlasmaPistol = ac_weapon.AC_WEAPON(12, 1, 3, 8, 3, 2, name = "PlasmaPistol")
+        Meltagun = ac_weapon.AC_WEAPON(12, 1, 3, 9, 4, 'd6', name = "Meltagun", MELTA = 2)
+        Grenade = ac_weapon.AC_WEAPON(24, 1, 3, 9, 2, 'd3', name = "Grenade")
+        
+        if name == 'FRFSRF' or name == 'BASE' or name == 'T_AIM':
             TempestorPrime = ac_unit.AC_UNIT(*base_stats, [PlasmaPistol], *abilities, **kwabilities)
         else:
             TempestorPrime = ac_unit.AC_UNIT(*base_stats, [], *abilities, **kwabilities)
@@ -59,39 +65,40 @@ if __name__ == '__main__':
         Voxer = ac_unit.AC_UNIT(*base_stats, [HotShotLaspistol], *abilities, **kwabilities)
         Volley = ac_unit.AC_UNIT(*base_stats, [HotShotVolleyGun], *abilities, **kwabilities)
         Medic = ac_unit.AC_UNIT(*base_stats, [HotShotLaspistol, HotShotLasgun], *abilities, **kwabilities)
+        Grenadier = ac_unit.AC_UNIT(*base_stats, [Grenade], *abilities, **kwabilities)
         
         BigScionSquad = ac_squad.AC_SQUAD([(TempestorPrime, 1),
                             (Tempestor, 1),
                             (Voxer, 1),
-                            (Volley, 1),
+                            (Grenadier, 1),
                             (PlasmaGunner, 3),
-                            (MeltaGunner, 3),
+                            (Volley, 3),
                             (Lasgunner, 4),
                             (Medic, 1)
                             ])
         
-        SpaceMarine = ac_unit.AC_UNIT(6, 4, 3, 0, 2, 6, 2, [], ac_weapon.AC_WEAPON.IN_COVER)
-        Terminator = ac_unit.AC_UNIT(5, 5, 3, 4, 3, 6, 2, [], ac_weapon.AC_WEAPON.IN_COVER)
-        PlagueMarine = ac_unit.AC_UNIT(6, 6, 3, 0, 2, 6, 2, [], ac_weapon.AC_WEAPON.IN_COVER)
-        TankT103p = ac_unit.AC_UNIT(6, 10, 3, 0, 11, 6, 3, [], ac_weapon.AC_WEAPON.IN_COVER)
-        TankT112p = ac_unit.AC_UNIT(6, 11, 2, 0, 11, 6, 3, [], ac_weapon.AC_WEAPON.IN_COVER)
-        TankT112p4pp = ac_unit.AC_UNIT(6, 11, 2, 4, 11, 6, 3, [], ac_weapon.AC_WEAPON.IN_COVER)
+        targets = [ac_unit.AC_UNIT(6, 4, 3, 0, 2, 6, 2, []),
+                   ac_unit.AC_UNIT(6, 4, 3, 0, 2, 6, 2, [], ac_weapon.AC_WEAPON.IN_COVER), 
+                   ac_unit.AC_UNIT(6, 6, 3, 0, 2, 6, 2, [], ac_weapon.AC_WEAPON.IN_COVER),
+                   ac_unit.AC_UNIT(5, 5, 3, 4, 3, 6, 2, []),
+                   ac_unit.AC_UNIT(5, 5, 3, 4, 3, 6, 2, [], ac_weapon.AC_WEAPON.IN_COVER),                   
+                   ac_unit.AC_UNIT(6, 10, 3, 0, 11, 6, 3, [], ac_weapon.AC_WEAPON.IN_COVER),
+                   ac_unit.AC_UNIT(6, 11, 2, 0, 11, 6, 3, [], ac_weapon.AC_WEAPON.IN_COVER),
+                   ac_unit.AC_UNIT(6, 11, 2, 4, 11, 6, 3, [], ac_weapon.AC_WEAPON.IN_COVER)
+                   ]
         
-        N = 10000
+        N = 100000
         
-        twsm = BigScionSquad._get_data_damage_single_target_with_wounds(SpaceMarine, 9.1, N)
-        twpm = BigScionSquad._get_data_damage_single_target_with_wounds(PlagueMarine, 9.1, N)
-        twt = BigScionSquad._get_data_damage_single_target_with_wounds(Terminator, 9.1, N)        
-        twtank1 = BigScionSquad._get_data_damage_single_target_with_wounds(TankT103p, 9.1, N)
-        twtank2 = BigScionSquad._get_data_damage_single_target_with_wounds(TankT112p, 9.1, N)
-        twtank3 = BigScionSquad._get_data_damage_single_target_with_wounds(TankT112p4pp, 9.1, N)
-        
-        DATA.append([twsm, twpm, twt, twtank1, twtank2, twtank3])
+        d = []
+        for target in targets:
+            d.append(BigScionSquad._get_data_damage_single_target_with_wounds(target, 9.1, N))
+        DATA.append(d)
+                        
         print(f"Done with {name}")
     
-    plot_series( DATA, plt.gca(), LABELS,  ['on SM', 'on plague', 'on termi', 'tank t10 3+', 'tank t11 2+', 'tank t11 2+ 4++'], between = False)
+    plot_series( DATA, plt.gca(), LABELS,  ['on SM', 'on SM cover', 'on plague cover', 'on termi', 'on termi cover', 'tank t10 3+', 'tank t11 2+', 'tank t11 2+ 4++'], between = False)
     plt.grid()
     plt.xlabel("Models killed")
-    plt.title("Scion-bombs on different targets")
+    plt.title("Scion-bombs on different targets (volly+gren)")
     plt.show()
     
